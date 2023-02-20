a56503f0d99b567ce30c779ceaaf1d6e32ebfe8c
michielstock <michielfmstock@gmail.com>
2023-02-20 17:49:57 +0000

    :ambulance: fix perm

--- src/Xicor.jl
+++ src/Xicor.jl
@@ -11,17 +11,18 @@ are no ties in `Y`, `noties` can be set to `true`, which speeds up computation.
 
 `ξ` is an alias for this function.
 """
-function xicor(X::AbstractVector, Y::AbstractVector; rank=tiedrank, noties=false)
+function xicor(X::AbstractVector, Y::AbstractVector; rank=denserank, noties=false)
     @assert length(X) == length(Y) "length `X` and `Y` missmatch"
     n = length(Y)
-    xind = sortperm(X)
-    r = rank(xind, by=i->Y[i])
+    xsortperm = sortperm(X)
+    # todo: carfully check the rank function
+    r = rank(xsortperm, by=i->Y[i])
     if !noties
-        l = rank(xind, by=i->Y[i], rev=true)
+        l = rank(xsortperm, by=i->Y[i], rev=true)
     end
     ξn = 0.0
     for i in 1:n-1
-        ξn += (abs(r[i+1] - r[i]))
+        ξn += abs(r[i+1] - r[i])
     end
     ξn *= noties ? 3 / (n^2 - 1) : n / 2sum(li->li*(n-li), l)
     ξn = 1 - ξn
