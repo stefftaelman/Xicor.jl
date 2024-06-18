0b931ce33a68e9db83e1f2a5a0957937032a45a4
stefftaelman <stefftaelman@gmail.com>
2024-06-18 09:55:08 +0200

    fix: wrong ranking function in docs

--- src/Xicor.jl
+++ src/Xicor.jl
@@ -6,7 +6,7 @@ using Distributions, Random, StatsBase
     xicor(X, Y; rank=tiedrank, noties=false)
 
 Computes the asymmetric ξ correlation coefficient sbetween two vectors `X` and `Y`.
-`rank` species how the rank is computed, default uses `tiedrank`. If there
+`rank` species how the rank is computed, default uses `denserank`. If there
 are no ties in `Y`, `noties` can be set to `true`, which speeds up computation.
 
 `ξ` is an alias for this function.
